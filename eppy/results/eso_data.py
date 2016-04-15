b649cfdce7dba80563da6e859803837a902058f4
Jamie Bull <jamie.bull@oco-carbon.com>
2016-04-15 11:34:05 +0200

    Add tests for esoreader wrapper

---
+++ eppy/results/eso_data.py
@@ -0,0 +1,16 @@
+test_results = """Program Version,EnergyPlus-Windows-64 8.1.0.009, YMD=2016.01.27 08:47
+1,5,Environment Title[],Latitude[deg],Longitude[deg],Time Zone[],Elevation[m]
+2,6,Day of Simulation[],Month[],Day of Month[],DST Indicator[1=yes 0=no],Hour[],StartMinute[],EndMinute[],DayType
+3,3,Cumulative Day of Simulation[],Month[],Day of Month[],DST Indicator[1=yes 0=no],DayType  ! When Daily Report Variables Requested
+4,2,Cumulative Days of Simulation[],Month[]  ! When Monthly Report Variables Requested
+5,1,Cumulative Days of Simulation[] ! When Run Period Report Variables Requested
+10,1,ZONE TEST 1,Zone Air System Sensible Cooling Rate [W] !Hourly
+11,1,ZONE TEST 2,Zone Air System Sensible Cooling Rate [W] !Hourly
+End of Data Dictionary
+1,CHICAGO OHARE INTL AP ANN HTG 99% CONDNS DB,  54.96,  -1.57,   0.00,  32.00
+2,1, 1,21, 0, 1, 0.00,60.00,WinterDesignDay
+10,10.0
+11,20.0
+End of Data
+ Number of Records Written=        1296
+"""
